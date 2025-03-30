b679ec34a0afdad5101e76e57f79e2c179699f3c
saravanan3004 <sarausa045@gmail.com>
2025-03-31 00:56:49 +0530

    Logging and Exceptions

--- src/exception.py
+++ src/exception.py
@@ -15,12 +15,4 @@ class CustomeException(Exception):
 
     def __str__(self):
         return self.error_msg
-    
-if __name__=="__main__":
-
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Division 1 by zero")
-        raise CustomeException(e,sys)
         
