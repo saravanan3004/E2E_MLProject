926547ae4ebfaedf77c6fb3bb854dd2101aaef3c
saravanan3004 <sarausa045@gmail.com>
2025-03-31 00:54:06 +0530

    Logging and Exception

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+def error_msg_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_msg="Error Occured in Python script name [{0}] Line Number [{1}] error msg [{2}]".format(
+    file_name, exc_tb.tb_lineno,str(error))
+    return error_msg
+
+class CustomeException(Exception):
+    def __init__(self,error_msg,error_detail:sys):
+        super().__init__(error_msg)
+        self.error_msg = error_msg_detail(error_msg,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_msg
+    
+if __name__=="__main__":
+
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Division 1 by zero")
+        raise CustomeException(e,sys)
+        
