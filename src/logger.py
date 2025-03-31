d1c3d4ff4c0a317cf6dc98b6532443f8837ea6f4
saravanan3004 <sarausa045@gmail.com>
2025-04-01 01:52:03 +0530

    first commit

--- src/logger.py
+++ src/logger.py
@@ -2,15 +2,22 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%m-%d-%Y-%H:%M:%S')}.log"
-logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Generate a unique log file name based on the current timestamp
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
+# Create a directory to store log files
+logs_path = os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,mode=0o700,exist_ok=True)
+
+# Define the full path for the log file
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
+# Configure logging settings
 logging.basicConfig(
-
     filename=LOG_FILE_PATH,
-    format='%(asctime)s %(levelname)-8s %(name)-15s %(message)s',
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO
 )
+
+#if __name__=="__main__":
+#    logging.info("Logging has Started")
